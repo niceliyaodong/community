9064814a19f217dfa6b3c652f41cdf21c8e9874c
DESKTOP-PCOITER\liyaodong <1877367503@qq.com>
2019-10-09 18:01:01 +0800

    配置文件

--- src/main/java/com/liyaod/community/community/controller/AuthorizeController.java
+++ src/main/java/com/liyaod/community/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.liyaod.community.community.entity.AccessTokenEntity;
 import com.liyaod.community.community.entity.GithubUserEntity;
 import com.liyaod.community.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,13 +19,20 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state) {
         AccessTokenEntity accessTokenEntity = new AccessTokenEntity();
-        accessTokenEntity.setClient_id("25937c33817d0cbfe4ef");
-        accessTokenEntity.setClient_secret("73e40515a345fc6e0c5fdad4816a5bf88ea82d3a");
+        accessTokenEntity.setClient_id(clientId);
+        accessTokenEntity.setClient_secret(clientSecret);
         accessTokenEntity.setCode(code);
-        accessTokenEntity.setRedirect_uri("http://localhost:8889/callback");
+        accessTokenEntity.setRedirect_uri(redirectUri);
         accessTokenEntity.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenEntity);
         GithubUserEntity user = githubProvider.getUser(accessToken);
